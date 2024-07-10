5dd4c4808761f2c024dc10904e713f40573596b5
nrfn <59220732+nrfn@users.noreply.github.com>
2024-07-11 00:40:56 +0900

    self-introduction.cs

---
+++ Exercise/self-introduction.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "nrfn";
+        string language = "C#";
+        string hitokoto = "freelance";
+        Console.WriteLine("name: " + name);
+        Console.WriteLine("language: " + language);
+        Console.WriteLine("hitokoto: " + hitokoto);
+    }
+}
